4fac2a1a0fc42c78fc7e9202d7cbc915753076ba
Usman Mehmood <bajwa345@gmail.com>
2022-07-09 21:57:37 +0500

    Add files via upload

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -9,6 +9,35 @@ exports.dbConfig = {
     driver: "tedious",
     //stream: false,
     //options: { encrypt: false, enableArithAbort: false, instanceName: 'SQLEXPRESS' },
-    options: { encrypt: true, enableArithAbort: false },
-    pool: { max: 900, min: 0, idleTimeoutMills: 300000 },
+    options: { encrypt: true, packetSize: 28368, enableArithAbort: false },
+    pool: { max: 600, min: 0, idleTimeoutMills: 20000 },
   };
+
+  /*exports.dbConfig = {
+    server: 'SQL5105.site4now.net',
+    database: "db_a8877c_dbvoogle",
+    user: "db_a8877c_dbvoogle_admin",
+    password: "Zxcv1234@",
+    port: 1433,
+    requestTimeout: 300000,
+    connectionTimeout: 300000,
+    //driver: "tedious",
+    //stream: false,
+    //options: { encrypt: false, enableArithAbort: false, instanceName: 'SQLEXPRESS' },
+    options: { encrypt: false, enableArithAbort: false },
+    pool: { max: 100, min: 0, idleTimeoutMills: 300000 },
+  };*/
+
+/*exports.dbConfig = {
+    server: 'localhost',
+    database: "db-voogle",
+    user: "sa",
+    password: "123456",
+    port: 1433,
+    requestTimeout: 300000,
+    connectionTimeout: 300000,
+    driver: "tedious",
+    //stream: false,
+    options: { encrypt: false, enableArithAbort: false, instanceName: 'SQLEXPRESS' },
+    pool: { max: 100, min: 0, idleTimeoutMills: 300000 },
+  };*/
